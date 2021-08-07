abcffa18d036e68c5f138b94c5e323dbcd67ce98
Pedro Natal <pedro.natal@m4u.com.br>
2021-08-07 16:10:36 -0300

    Inserindo produtos via flyway

--- src/main/java/com/natal/catalogservice/facade/ProductService.java
+++ src/main/java/com/natal/catalogservice/facade/ProductService.java
@@ -14,6 +14,8 @@ public interface ProductService {
     List<ProductTO> findProducts();
     List<TypeTO> findTypes();
     ProductTO findProductByCode(String productCode);
+    List<ProductTO> findProductsByTypeName(String typeName);
+    void insertProduct(Product product);
     void persistProducts(List<ProductTO> products);
     void persistType(TypeTO type);
 }
--- src/main/resources/db/migration/V3__DML_INSERT_PRODUCTS.sql
+++ src/main/resources/db/migration/V3__DML_INSERT_PRODUCTS.sql
@@ -1,9 +1,9 @@
---insert into product (code, name, type, price, available)
---values
---('XB21-4C', 'Contra Baixo Tagima XB21 4C', (select id from type where name = 'INSTRUMENTO'), 99990, true),
---('BD1-4C', 'Contra Baixo SX BD1 4C', (select id from type where name = 'INSTRUMENTO'), 89990, true),
---('Lps-230', 'Guitarra Les Paul Lps-230', (select id from type where name = 'INSTRUMENTO'), 117990, true),
---('BM800', 'Microfone BM800', (select id from type where name = 'ACESSORIO'), 16990, true),
---('SCAR_SOLO3', 'Interface de Audio Scarlett Solo 3', (select id from type where name = 'ACESSORIO'), 119990, true),
---('4C-CB', 'Kit 4 Cordas Contra Baixo', (select id from type where name = 'ACESSORIO'), 11990, true),
---('6C-CB', 'Kit 6 Cordas Guitarra', (select id from type where name = 'ACESSORIO'), 9990, true);
+insert into product (code, name, type_id, price, available, registry_date, last_update)
+values
+('XB21-4C', 'Contra Baixo Tagima XB21 4C', (select id from type where name = 'INSTRUMENTO'), 99990, true, now(), now()),
+('BD1-4C', 'Contra Baixo SX BD1 4C', (select id from type where name = 'INSTRUMENTO'), 89990, true, now(), now()),
+('Lps-230', 'Guitarra Les Paul Lps-230', (select id from type where name = 'INSTRUMENTO'), 117990, true, now(), now()),
+('BM800', 'Microfone BM800', (select id from type where name = 'ACESSORIO'), 16990, true, now(), now()),
+('SCAR_SOLO3', 'Interface de Audio Scarlett Solo 3', (select id from type where name = 'ACESSORIO'), 119990, true, now(), now()),
+('4C-CB', 'Kit 4 Cordas Contra Baixo', (select id from type where name = 'ACESSORIO'), 11990, true, now(), now()),
+('6C-CB', 'Kit 6 Cordas Guitarra', (select id from type where name = 'ACESSORIO'), 9990, true, now(), now());
